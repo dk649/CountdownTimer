05821f3c45112fe7d99a6ee464adb44b68e6b3b6
dk649 <58308212+dk649@users.noreply.github.com>
2020-11-24 15:44:03 +0000

    updates

---
+++ script.js
@@ -0,0 +1,35 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
+
+
+    const daysElement = document.getElementById('days');
+    const hoursElement = document.getElementById('hours');
+    const minutesElement = document.getElementById('minutes');
+    const secondsElement = document.getElementById('seconds');
+    const newYears = "1 Jan 2021";
+
+    function countdown() {
+        const newYearsDate = new Date(newYears);
+        const currentDate = new Date();
+
+        const totalSeconds = (newYearsDate - currentDate) / 1000;
+        const hours = Math.floor(totalSeconds /3600) % 24;
+
+        const days = Math.floor(totalSeconds / 3600 / 24);
+        const minutes = Math.floor(totalSeconds / 60) % 60;
+        const seconds = Math.floor(totalSeconds) % 60;
+
+        console.log(days, hours, minutes, seconds);
+
+        daysElement.innerHTML = days;
+        hoursElement.innerHTML = hours;
+        minutesElement.innerHTML = minutes;
+        secondsElement.innerHTML = seconds;
+
+    }
+
+    countdown();
+
+        setInterval(countdown, 1000);
+    
+});
